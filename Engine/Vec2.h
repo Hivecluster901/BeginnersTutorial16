c607f19648ee6facf4123efb9d7325d585b7fbab
Minjun1234 <bill981212@gmail.com>
2020-03-03 20:53:15 +0900

    Subtraction, subtraction -=, length, lengthsq, normalize, getnormalized functions updated

--- Engine/Vec2.h
+++ Engine/Vec2.h
@@ -9,6 +9,12 @@ public:
 	Vec2& operator+=(const Vec2& rhs);// Vec2&
 	Vec2 operator*(float rhs);
 	Vec2& operator*=(float rhs);
+	Vec2 operator-(const Vec2 rhs)const;
+	Vec2& operator-=(const Vec2& rhs);
+	float GetLength() const;
+	float GetLengthSq() const;
+	Vec2& Normalize() ;
+	Vec2 GetNormailzed() ;
 public:
 	float x;
 	float y;
